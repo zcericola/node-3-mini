d8e3511bed5fa13efd1d13bd7a60ac963856fa49
= <webdev.jameslemire@gmail.com>
2017-06-21 09:39:05 -0600

    JL master - Update front-end

--- public/src/components/ChatWindow/Message/Message.js
+++ public/src/components/ChatWindow/Message/Message.js
@@ -32,7 +32,6 @@ export default class Message extends Component {
   render() {
     const { id, text, time, edit, remove } = this.props;
     const { editting } = this.state;
-    console.log( id, text );
     return (
       <div className="Message__container">
         <span className="Message__time">{time}</span>
